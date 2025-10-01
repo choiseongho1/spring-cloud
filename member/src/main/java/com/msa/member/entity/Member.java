73bd937864b6041ad46948c3bee6e9c3bcc9d259
choiseongho95 <95_csh@naver.com>
2025-10-01 13:17:39 +0900

    feat: 프로젝트 기본 설정 파일 추가 및 .gitignore 설정

---
+++ member/src/main/java/com/msa/member/entity/Member.java
@@ -0,0 +1,19 @@
+package com.msa.member.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor @AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
